03fbccdd41f738e82aa546c88681fb0a7ead94b4
Jose Franco <jfranco@ikono.com.co>
2023-09-14 14:26:02 -0500

    feat(node): :sparkles: Add asr to get data node

--- ivrflow/nodes/get_data.py
+++ ivrflow/nodes/get_data.py
@@ -1,15 +1,21 @@
-from typing import TYPE_CHECKING, Dict
+from time import time
+from typing import TYPE_CHECKING, Dict, Union
+
+from sqids import Sqids
 
 from ..channel import Channel
 from ..models import GetData as GetDataModel
+from ..types import MiddlewareType
 from .switch import Switch
 
 if TYPE_CHECKING:
-    from ..middlewares import TTSMiddleware
+    from ..middlewares import ASRMiddleware, TTSMiddleware
+
+sqids = Sqids()
 
 
 class GetData(Switch):
-    middleware: "TTSMiddleware" = None
+    middleware: Union["TTSMiddleware", "ASRMiddleware"] = None
 
     def __init__(self, get_data_content: GetDataModel, channel: Channel, default_variables: Dict):
         super().__init__(get_data_content, channel, default_variables)
@@ -34,18 +40,45 @@ class GetData(Switch):
     async def run(self):
         self.log.info(f"Channel {self.channel.channel_uniqueid} enters input node {self.id}")
 
+        middleware_type = MiddlewareType(self.middleware.type) if self.middleware else None
+
         sound_path = self.file
-        if self.middleware and self.text:
+        if middleware_type == MiddlewareType.tts and self.text:
             await self.channel.set_variable("tts_text", self.text)
             await self.middleware.run()
             sound_path = self.middleware.sound_path
+            await self.channel.set_variable("tts_sound_path", sound_path)
 
-        variable = await self.asterisk_conn.agi.get_data(
-            filename=sound_path,
-            timeout=self.timeout,
-            max_digits=self.max_digits,
-        )
+        if middleware_type == MiddlewareType.asr:
+            record_suffix = sqids.encode([int(time())])
+            record_filename, record_format = (
+                f"{self.channel.channel_uniqueid}_{record_suffix}",
+                "wav",
+            )
 
-        await self.channel.set_variable(self.content.variable, variable.result)
+            if sound_path:
+                await self.asterisk_conn.agi.stream_file(sound_path)
+            result = await self.asterisk_conn.agi.record_file(
+                filename=record_filename,
+                audio_format=record_format,
+                escape_digits="#",
+                timeout=20000,
+                silence=2000,
+            )
+            await self.channel.set_variable("asr", "{}")
+            await self.channel.set_variable("asr_file_path", f"{record_filename}.{record_format}")
+            await self.middleware.run()
+            result = self.middleware.text
+            await self.channel.set_variable("asr_text", result)
+
+        else:
+            variable = await self.asterisk_conn.agi.get_data(
+                filename=sound_path,
+                timeout=self.timeout,
+                max_digits=self.max_digits,
+            )
+            result = variable.result
+
+        await self.channel.set_variable(self.content.variable, result)
 
         await super().run()
